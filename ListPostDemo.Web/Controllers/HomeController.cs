79301d18e03d46bec89fd7f12c7c2421fd60c0a1
Avrumi Friedman <info@lakewoodprogramming.com>
2023-03-27 22:21:08 -0400

    Added HW instructions

--- ListPostDemo.Web/Controllers/HomeController.cs
+++ ListPostDemo.Web/Controllers/HomeController.cs
@@ -48,3 +48,11 @@ namespace ListPostDemo.Web.Controllers
         }
     }
 }
+
+//create an mvc application that displays a list of people. On top of the list
+//have a link that takes you to a page that allows the user to enter multiple
+//people. When that page loads, there should be one row of textboxes (first name,
+//lastname and age) with a button on top that says "Add". When Add is clicked,
+//another row of textboxes should appear. Beneath those textboxes, there should
+//be a submit button, that when clicked, adds all those people to the database
+//and then redirects the user back to the home page.
